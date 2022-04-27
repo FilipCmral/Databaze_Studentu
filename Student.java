efe9a37653d992609bd869a5e34c568eed0bfbf7
FilipCmral <64127941+FilipCmral@users.noreply.github.com>
2022-04-27 17:00:33 +0200

    Delete Student.java

--- Student.java
+++
@@ -1,6 +0,0 @@
-package com.Filip;
-
-public interface Student {
-    //nějeké vlastnosti,funkce apod. co mají bakaláři, doktoranti i magistři
-    //kazdy student musi mit funkci na spocitani vazeneho prumeru podle predmetu, co absolvoval
-}
