378778332806899a4b4f895d011526a401015671
FilipCmral <64127941+FilipCmral@users.noreply.github.com>
2022-04-27 17:00:24 +0200

    Delete Predmet.java

--- Predmet.java
+++
@@ -1,9 +0,0 @@
-package com.Filip;
-
-public class Predmet {
-    private String jmeno;
-    private String vyucujici; //toto pak nebude string ale predmet ucitel
-    private int pocetKreditu;
-
-    //kazdy predmet je jeste prirazen fakulte
-}
